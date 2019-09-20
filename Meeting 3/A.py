ad9d636551cabed97d959659e23df139197df758
Richard <jsksosdgkd29y@gmail.com>
2019-09-20 19:08:16 -0400

    A.py fails on the 12th test case

---
+++ Meeting 3/A.py
@@ -0,0 +1,57 @@
+record = input()
+rank = 25
+stars = 0
+previous = False
+streak = False
+
+for i in record:
+    if i == 'W':
+        stars += 1
+        if rank > 5 and previous:
+            if streak:
+                stars += 1
+            else:
+                streak = True
+        else: previous = True
+
+        if 21 <= rank <= 25:
+            if stars > 2:
+                stars -= 2
+                rank -= 1
+        elif 16 <= rank <= 20:
+            if stars > 3:
+                stars -= 3
+                rank -= 1
+        elif 11 <= rank <= 15:
+            if stars > 4:
+                stars -= 4
+                rank -= 1
+        elif 1 <= rank <= 10:
+            if stars > 5:
+                stars -= 5
+                rank -= 1
+        else:
+            break
+    else:
+        previous = False
+        streak = False
+        if rank > 20:
+            pass
+        elif 16 <= rank <= 20:
+                stars -= 1
+                if stars == -1:
+                    stars += 3
+                    rank += 1
+        elif 11 <= rank <= 15:
+                if stars == -1:
+                    stars += 4
+                    rank += 1
+        elif 1 <= rank <= 10:
+                if stars == -1:
+                    stars += 5
+                    rank += 1
+        else:
+                break
+if rank > 0:
+    print(rank)
+else: print("Legend")
---
+++ Meeting 3/E.py
@@ -0,0 +1,7 @@
+temp = input().split()
+x = int(temp[0])
+y = int(temp[1])
+lines = int(input())
+here = []
+for i in range(lines):
+    here.append(input())
